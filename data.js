6c0248df3fde84f07730c4c4918d3cf3f1d5b5f8
Leon <leonard.avp@gmail.com>
2021-03-06 18:24:07 -0400

    Menus de postres y cafes

---
+++ data.js
@@ -0,0 +1,135 @@
+const cafes = [
+    {
+        nombre:'Espresso',
+        precio:'1.99€',
+        imgURL:'https://www.takeitcoffee.com/wp-content/uploads/2020/05/espresso.jpg'
+    },
+    {
+        nombre:'Ristretto',
+        precio:'0.99€',
+        imgURL:'https://emozionecoffee.files.wordpress.com/2014/06/cafe-ristretto.jpg'
+    },
+    {
+        nombre:'Doble Espresso',
+        precio:'3.99€',
+        imgURL:'https://cdn.joinnus.com/files/2020/06/LYFk3ZLo2WpM8MC.jpg'
+    },
+    {
+        nombre:'Espresso Largo',
+        precio:'4.99€',
+        imgURL:'https://www.philips.com.pe/c-dam/b2c/category-pages/Household/coffee/master/faq/cups/Productimg_Lungo.png'
+    },
+    {
+        nombre:'Macchiato',
+        precio:'2.99€',
+        imgURL:'https://juani.cafe/wp-content/uploads/2020/04/que-es-un-cafe-macchiato.jpg'
+    },
+    {
+        nombre:'Cortado',
+        precio:'1.99€',
+        imgURL:'https://perfectdailygrind.com/es/wp-content/uploads/sites/2/2020/03/Cortadito-1.png'
+    },
+    {
+        nombre:'Capuccino',
+        precio:'2.99€',
+        imgURL:'https://img.freepik.com/foto-gratis/taza-cafe-leche_74190-104.jpg?size=626&ext=jpg'
+    },
+    {
+        nombre:'Latte',
+        precio:'3.99€',
+        imgURL:'https://cdn.kiwilimon.com/recetaimagen/36986/46349.jpg'
+    },
+    {
+        nombre:'Café Lagrima',
+        precio:'2.99€',
+        imgURL:'https://pbs.twimg.com/media/CgalaPTWwAA918w.jpg'
+    },
+    {
+        nombre:'Café con Leche',
+        precio:'2.99€',
+        imgURL:'https://cdn.kiwilimon.com/recetaimagen/15262/th5-640x640-7456.jpg'
+    },
+    {
+        nombre:'Café Bombon',
+        precio:'3.99€',
+        imgURL:'https://www.hogarmania.com/archivos/201501/cafe-bombon-668x400x80xX.jpg'
+    },
+    {
+        nombre:'Café Americano',
+        precio:'1.99€',
+        imgURL:'https://i.pinimg.com/originals/0f/af/9e/0faf9e3d38a3d59e3ac206c42519a59e.jpg'
+    },
+    {
+        nombre:'Café Vienes',
+        precio:'3.99€',
+        imgURL:'https://www.barexpres.com/wp-content/uploads/2011/06/cafe-vienes-banner.jpg'
+    },
+    {
+        nombre:'Moka',
+        precio:'3.99€',
+        imgURL:'https://cdn.kiwilimon.com/recetaimagen/36987/th5-640x426-46351.jpg'
+    },
+]
+
+const panes = [
+    {
+        nombre:'Pain au chocolat',
+        precio:'5.99€',
+        imgURL:'https://coteathome.co.uk/wp-content/uploads/Cote-At-Home-Pain-au-Chocolat-2-x-65g-5f37a3283b65c.jpeg'
+    },
+    {
+        nombre:'Croissant',
+        precio:'4.99€',
+        imgURL:'https://www.hogarmania.com/archivos/202101/cocina-recetas-como-hacer-croissants-cruasanes-caseros-668x400x80xX.jpg'
+    },
+    {
+        nombre:'Baguette',
+        precio:'1.99€',
+        imgURL:'https://i.ytimg.com/vi/mI6K-4_JPrM/maxresdefault.jpg'
+    },
+    {
+        nombre:'Macaron',
+        precio:'0.99€',
+        imgURL:'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/elgranchef/M/Macarons.jpg'
+    },
+    {
+        nombre:'Brioche',
+        precio:'9.99€',
+        imgURL:'https://www.recetasderechupete.com/wp-content/uploads/2020/01/Brioche.jpg'
+    },
+    {
+        nombre:'Cannelé',
+        precio:'3.99€',
+        imgURL:'https://www.eatlivetravelwrite.com/wp-content/uploads/2020/01/IMG_4858.jpg'
+    },
+    {
+        nombre:'Chausson aux pommes',
+        precio:'6.99€',
+        imgURL:'https://blog.monmagasingeneral.com/wp-content/uploads/2019/01/recette-cuisine-viennoiserie-chaussons-aux-pommes-1270x846.jpg'
+    },
+    {
+        nombre:'Financier',
+        precio:'1.99€',
+        imgURL:'https://www.demoslavueltaaldia.com/sites/default/files/financiers.jpg'
+    },
+    {
+        nombre:'Tarte Tatin',
+        precio:'8.99€',
+        imgURL:'https://img.cuisineaz.com/610x610/2016-09-19/i1730-la-tarte-tatin-pommes-caramelisees.jpg'
+    },
+    {
+        nombre:'Tarte au citron',
+        precio:'7.99€',
+        imgURL:'https://cdn.pratico-pratiques.com/app/uploads/sites/7/2019/07/25155059/tarte-au-citron-a-l-autocuiseur.jpg'
+    },
+    {
+        nombre:'Croque-monsieur',
+        precio:'4.99€',
+        imgURL:'https://www.papillesetpupilles.fr/wp-content/uploads/2018/10/Croque-Monsieur-%C2%A9D.-Pimborough-shutterstock.jpg'
+    },
+    {
+        nombre:'Éclair',
+        precio:'1.99€',
+        imgURL:'https://www.demoslavueltaaldia.com/sites/default/files/styles/recetas_listado/public/eclairs_de_chocolate_1.jpg'
+    }
+]
